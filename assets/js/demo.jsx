f162542582a864bfa9cad312c8f42c7dacc605db
CastleKey <izaak.lofgren@gmail.com>
2018-01-30 12:48:27 -0500

    Made functional reset button, click counter, and delay on two tiles not matching

--- assets/js/demo.jsx
+++ assets/js/demo.jsx
@@ -12,6 +12,7 @@ class Demo extends React.Component {
     this.state = {
       delts: Array(16).fill(0),
       locks: Array(16).fill(0),
+      clickCount: 0,
       stepNumber: 0,
       letterOne: "",
       letterTwo: "",
@@ -26,6 +27,7 @@ class Demo extends React.Component {
     var lockChange = this.state.locks;
     var deltsChange = this.state.delts;
 
+    var clickChange = this.state.clickCount + 1;
     var stepChange = this.state.stepNumber;
     var letterChange = this.state.letterOne;
     var letterChange2 = this.state.letterTwo;
@@ -55,6 +57,7 @@ class Demo extends React.Component {
           side: 0,
           delts: deltsChange,
           locks: lockChange,
+          clickCount: clickChange,
           stepNumber: stepChange,
           letterOne: letterChange,
           letterTwo: letterChange2,
@@ -74,12 +77,15 @@ class Demo extends React.Component {
         side: 0,
         delts: deltsChange,
         locks: lockChange,
+        clickCount: clickChange,
         stepNumber: stepChange,
         letterOne: letterChange,
         letterTwo: letterChange2,
         numberOne: numberChange,
         numberTwo: numberChange2
       })
+
+      setTimeout( () => this.wait(), 1000);
     }
     else if (this.state.stepNumber == 2) {
       deltsChange[minus1] = 1;
@@ -95,6 +101,7 @@ class Demo extends React.Component {
         side: 0,
         delts: deltsChange,
         locks: lockChange,
+        clickCount: clickChange,
         stepNumber: stepChange,
         letterOne: letterChange,
         letterTwo: letterChange2,
@@ -119,17 +126,64 @@ class Demo extends React.Component {
         side: 0,
         delts: deltsChange,
         locks: lockChange,
+        clickCount: clickChange,
         stepNumber: stepChange,
         letterOne: letterChange,
         letterTwo: letterChange2,
         numberOne: numberChange,
         numberTwo: numberChange2
       });
+
+
+
     }
   }
 
+  wait() {
+    var deltsChange = this.state.delts;
+    var stepChange = this.state.stepNumber;
+
+    var numberChange1 = this.state.numberOne;
+    var numberChange2 = this.state.numberTwo;
+
+    var minus1 = numberChange1 - 1;
+    var minus2 = numberChange2 - 1;
+
+    deltsChange[minus1] = 0;
+    deltsChange[minus2] = 0;
+
+    this.setState({
+      side: 0,
+      delts: deltsChange,
+      locks: this.state.locks,
+      clickCount: this.state.clickCount,
+      stepNumber: 0,
+      letterOne: "",
+      letterTwo: "",
+      numberOne: 20,
+      numberTwo: 25
+    });
+  }
+
+  reset() {
+    this.setState({
+      side: 0,
+      delts: Array(16).fill(0),
+      locks: Array(16).fill(0),
+      clickCount: 0,
+      stepNumber: 0,
+      letterOne: "",
+      letterTwo: "",
+      numberOne: 20,
+      numberTwo: 25,
+    });
+  }
+
+
   render() {
     var toggle = this.toggle.bind(this);
+    var reset = this.reset.bind(this);
+    var wait = this.wait.bind(this);
     return (
       <div>
         <div className="row">
@@ -156,6 +210,12 @@ class Demo extends React.Component {
           <Side show={this.state.delts[14] == 0} lock={this.state.locks[14] == 0} toggle={toggle} number={15} value={"G"}/>
           <Side show={this.state.delts[15] == 0} lock={this.state.locks[15] == 0} toggle={toggle} number={16} value={"H"}/>
         </div>
+        <div className="row">
+          <div id="side-0" className="side col">
+            <Button type="button" color="warning" onClick={ () => reset()}>Reset Game</Button>
+            <Button type="button" color="info" disabled>Click Count: {this.state.clickCount}</Button>
+          </div>
+        </div>
       </div>
     );
   }
@@ -165,7 +225,7 @@ function Side(params) {
   if (!params.lock) {
     return (
       <div id="side-0"className="side col">
-        <Button type="button" color="danger" number={params.number} onClick={ () => params.toggle(params.number, params.value) } disabled>
+        <Button type="button" color="success" number={params.number} onClick={ () => params.toggle(params.number, params.value) } disabled>
           {params.value}
         </Button>
       </div>
@@ -183,7 +243,7 @@ function Side(params) {
   else {
     return (
       <div id="side-0"className="side col">
-        <Button type="button" color="danger" number={params.number} onClick={ () => params.toggle(params.number, params.value) }>
+        <Button type="button" color="danger" number={params.number} onClick={ () => params.toggle(params.number, params.value) } disabled>
           {params.value}
         </Button>
       </div>
